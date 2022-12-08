12c38ab44012b4ae0a2c4dacd863e9f47fadfd4a
pohjie <0xArlet@gmail.com>
2022-12-08 09:49:45 +0800

    Remove red borders when passwords match

---
+++ signup-logic.js
@@ -0,0 +1,14 @@
+function checkPwdMatch() {
+  const pwd = document.getElementById('pwd');
+  const cfmPwd = document.getElementById('cfmPwd');
+
+  if (pwd.value === cfmPwd.value) {
+    pwd.setCustomValidity('');
+    pwd.classList.remove('error');
+    cfmPwd.classList.remove('error');
+  } else {
+    pwd.setCustomValidity('Passwords do not match');
+    pwd.classList.add('error');
+    cfmPwd.classList.add('error');
+  }
+}
